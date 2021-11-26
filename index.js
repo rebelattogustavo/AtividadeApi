e170fb8663e7b3790c2c746642c660c89462a4d0
Your Name <you@example.com>
2021-11-25 21:49:27 -0300

    Refatoração de código

--- index.js
+++ index.js
@@ -1,4 +1,176 @@
+const java = document.querySelector('body');
 
+
+let valoresArr = [];
+
+
+let button = document.createElement('button');
+button.className = 'button';
+button.innerText = 'Cadastro';
+
+let container = document.createElement('div');
+container.className = 'container';
+
+container.appendChild(button);
+
+java.appendChild(container);
+
+
+
+button.addEventListener('click', function(){
+    //Criando a div do modal
+    let containerModal = document.createElement('div');
+    containerModal.className = 'container-modal';
+
+    //Criando o modal
+    let modal = document.createElement('div');
+    modal.className = 'div-modal'
+    
+    //Add o modal na div
+    containerModal.appendChild(modal);
+
+    //Add a div no body
+    java.appendChild(containerModal);
+
+    java.style.backgroundColor = 'grey';
+    button.style.backgroundColor = 'grey';
+
+
+    let titulo = document.createElement('h2');
+    titulo.innerText = 'CADASTRÃO';
+    titulo.className = 'titulo-modal';
+
+
+    modal.appendChild(titulo);
+
+
+    let divNome = document.createElement('div');
+    divNome.className = 'div-nome';
+    
+
+    let nomeInput = document.createElement('input');
+    nomeInput.className = 'input-nome';
+    nomeInput.placeholder = 'Nome';;
+    
+
+    divNome.appendChild(nomeInput);
+    modal.appendChild(divNome);
+
+
+    let divSobrenome = document.createElement('div');
+    divSobrenome.className = 'div-nome';
+    
+    let login = document.createElement('input');
+    login.className = 'input-sobrenome';
+    login.placeholder = 'Login';
+    
+    divSobrenome.appendChild(login);
+    modal.appendChild(divSobrenome);
+
+
+    let divBoToesInferior = document.createElement('div');
+    divBoToesInferior.className = 'div-botoes-inferior';
+
+
+    let enviar = document.createElement('button');
+    enviar.className = 'botao-enviar';
+    enviar.innerText = 'Cadastrar';
+
+    modal.appendChild(enviar)
+
+    divBoToesInferior.appendChild(enviar);
+
+    let cancelar = document.createElement('button');
+    cancelar.className = 'botao-cancelar';
+    cancelar.innerText = 'Cancelar';
+    
+    divBoToesInferior.appendChild(cancelar);
+
+    
+    enviar.onclick = function(){
+        if(nomeInput.value == '' || login.value == ''){
+            const divVermelha =  document.createElement('div');
+            divVermelha.className = 'father-vermelha';
+
+
+            const vermelha = document.createElement('div');
+            vermelha.className = 'div-vermelha'
+            vermelha.innerHTML = '<p>HÁ ALGUM CAMPO VAZIO</p>';
+            vermelha.style.position = 'absolute';
+            vermelha.style.bottom = 0;
+
+            divVermelha.appendChild(vermelha);
+
+            java.appendChild(divVermelha);
+
+            let intervaloV = setTimeout(function(){
+                vermelha.remove()
+                
+            },3000)
+        }
+        
+        else if(nomeInput.value != '' && login.value != ''){
+            
+            let tabela = document.createElement('table')
+            tabela.className = 'tabela'
+            
+            java.appendChild(tabela)
+            
+            const divVerde = document.createElement('div');
+            divVerde.className = 'father-verde';
+            
+            const verde = document.createElement('div');
+            verde.className = 'div-verde';
+            verde.innerHTML = '<p>CADASTRO CONCLUÍDO</p>';
+            verde.style.position = 'absolute';
+            verde.style.bottom = 0;
+            
+            divVerde.appendChild(verde);
+
+            java.appendChild(divVerde);
+            
+            let intervalo = setTimeout(function(){
+                containerModal.remove();
+                
+                java.style.backgroundColor = 'transparent';
+                button.style.background = '#EFEFEF';
+                button.style.outline = 'none';
+            },1)
+
+            let intervaloV2 = setTimeout(function(){
+                verde.remove()
+                
+            },5000)
+
+            
+            let nome = document.querySelector('.input-nome')
+
+
+            let login = document.querySelector('.input-sobrenome')
+    
+
+
+            
+            usersList.push({name: nome.value, userName: login.value})
+            criarLista();
+
+        }
+        
+        
+        
+    }
+    
+    cancelar.onclick = function(){
+        containerModal.remove();
+        java.style.backgroundColor = 'transparent';
+        button.style.background = '#EFEFEF';
+        button.style.outline = 'none';
+
+    }
+    
+    modal.appendChild(divBoToesInferior)
+    
+})
 
 const usersList = [
     { name: 'Bruno Henrique', userName: 'brunohvc' },
@@ -27,6 +199,10 @@ console.log(usersList);
 
 
 function criarLista(){
+    const listaAtual = document.querySelector('.div-lista');
+    if(listaAtual){
+        listaAtual.remove()
+    }
 
     let divLista = document.createElement('div');
     divLista.className = 'div-lista';
